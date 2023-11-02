928beadabf424bff185099b193c6e4bc28209389
a <@>
2023-11-02 13:34:49 -0400

    lkvlijw

--- Class/Class 10/tree_lol.py
+++ Class/Class 10/tree_lol.py
@@ -30,3 +30,20 @@ class Tree:
         if not self._subtrees[0]._subtrees:
             return self
         return self._subtrees[0].find_leftmost_leaf_parent()
+
+
+class Foo:
+    @staticmethod
+    def foo():
+        print("hello")
+
+        Foo.bar()
+
+    # foo()
+
+    @staticmethod
+    def bar():
+        print("world")
+
+
+Foo.foo()
